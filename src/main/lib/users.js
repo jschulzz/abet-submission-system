e30508c2375d62cb74988adbb7f877113d218956
Chad Nelson <chadnelson@Chads-MacBook-Pro.local>
2019-10-28 22:03:30 -0400

    Wrote tests for the users.new function.

--- src/main/lib/users.js
+++ src/main/lib/users.js
@@ -1,14 +1,12 @@
 const User = require('../models/User')
 
-module.exports.new = async ({
-	username
-}) => {
+module.exports.new = async (username) => {
 	if(username == ""){
-        throw new Error("User must have username")
+        throw new Error('User must have username')
     }
     let user = await User.query().where('linkblue_username', username)
-    if (user !== null) {
-        throw new Error('user already exists')
+    if (user.length !== 0) {
+        throw new Error('User already exists')
     }
     let new_user = await User.query().insert({linkblue_username: username})
 
---
+++ src/test/cases/lib/users.js
@@ -0,0 +1,60 @@
+const users = require('../../../main/lib/users')
+const { expect } = require('../../chai')
+const sinon = require('sinon')
+
+// we use a sandbox so that we can easily restore all stubs created in that sandbox
+const sandbox = sinon.createSandbox();
+
+describe('Lib - Users', () =>{
+    describe('new', () => {
+        // this is ran after each unit test
+		afterEach(() => {
+			// this is needed to restore the CoursePortfolio model back to it's original state
+			// we don't want to break all future unit tests
+			sandbox.restore()
+        })
+        it('already exists', async () => {
+            // Arrange
+            const User = require('../../../main/models/User')
+            const input = 'user'
+            sandbox.stub(User, "query").returns({
+                where: sandbox.stub().returns([{
+                    "id": 1,
+                    "linkblue_username": "user"
+                }])
+            })
+            
+            // Act & Assert
+            await expect(users.new(input)).to.be.rejectedWith(Error, 'User already exists')
+        })
+
+        it('empty username', async () => {
+            //Arange
+            const input = ""
+
+            // Act & Assert
+            await expect(users.new(input)).to.be.rejectedWith(Error, 'User must have username')
+        })
+
+        it('add new user', async () => {
+            // Arrange
+            const expected_output = [{ "id": 1,
+                                    "linkblue_username": "user" }]
+            const User = require('../../../main/models/User')
+            const input = 'user'
+            sandbox.stub(User, "query").returns({
+                where: sandbox.stub().returns([]),
+                insert: sandbox.stub().returns([{
+                    "id": 1,
+                    "linkblue_username": "user"
+                }])
+            })
+
+            // Act
+            const actual_output = await users.new(input)
+
+            // Assert
+            expect(actual_output).to.deep.equal(expected_output)
+        })
+    })
+})
